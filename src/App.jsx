6ccbdab81f97f430f9a6af17f76d8b0c8ec1c1b9
PRANSHU SINGH THAKUR <pspranshusingh15@gmail.com>
2024-08-27 15:54:41 +0530

    changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import Dashboard from './components/Dashboard';
-
-// const App = () => {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     axios.get('http://localhost:5000/api/data')
-//       .then(response => setData(response.data))
-//       .catch(error => console.error(error));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Data Visualization Dashboard</h1>
-//       <Dashboard data={data} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 // frontend/src/App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
